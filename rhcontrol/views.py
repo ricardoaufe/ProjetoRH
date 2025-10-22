f70bc14b33c14f9cab28472a98b12cd36ade2854
ricardoaufe <rasf0831@gmail.com>
2025-10-22 17:57:48 -0300

    Font-awesome + namespacing + partials + criação de head (html padrão das páginas)

--- rhcontrol/views.py
+++ rhcontrol/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def dashboard(request):
+    return render(request, 'dashboard/pages/dashboard.html')
