405faabd2a878b3bf49253c9a8dabe2ab89c7b0b
ricardoaufe <rasf0831@gmail.com>
2025-11-19 18:20:26 -0300

    Configs iniciais para testes de django

--- rhcontrol/tests.py
+++ rhcontrol/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class RhcontrolTests(TestCase):
+    def test_rh_dashboard_url_is_corret(self):
+        dashboard_url = reverse('rhcontrol:rh_home')
+        self.assertEqual(dashboard_url, '/')
+
+        
