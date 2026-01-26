085268fe4dd5da8147d348786e3f6d61d78f6470
ricardoaufe <rasf0831@gmail.com>
2026-01-26 18:56:02 -0300

    Ajuste no STATIC_URL

--- rh/settings.py
+++ rh/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'rhcontrol/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
