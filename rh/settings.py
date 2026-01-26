fae7b555aa0b2feb1e0b7818ca8ac3bee568be87
ricardoaufe <rasf0831@gmail.com>
2026-01-26 19:51:17 -0300

    STATICFILES_DIRS

--- rh/settings.py
+++ rh/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'base_static',
+]
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
