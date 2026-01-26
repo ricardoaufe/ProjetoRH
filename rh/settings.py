01094af641a6081ed32d43725bdfefcfb9c064e4
ricardoaufe <rasf0831@gmail.com>
2026-01-26 20:14:07 -0300

    Mudança no gitignore

--- rh/settings.py
+++ rh/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'base_static',
 ]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
