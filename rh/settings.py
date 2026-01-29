2da5b63f7d77300ce0cfc57acfdc58d77a0502c0
ricardoaufe <rasf0831@gmail.com>
2026-01-29 16:28:07 -0300

    Ajuste no allowed_hosts

--- rh/settings.py
+++ rh/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://rh.gdlgestaosis.com.br']
 
 
 # Application definition
